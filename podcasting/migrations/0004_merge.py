4f3b7c58165d217625d284deedebdb57c550f432
Håkan Waara <hwaara@gmail.com>
2015-10-08 11:56:30 +0200

    add old merge migration

---
+++ podcasting/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('podcasting', '0003_auto_20150402_1539'),
+        #('podcasting', '0003_auto_20150402_1242'),
+    ]
+
+    operations = [
+    ]
